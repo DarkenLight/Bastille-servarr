2ab5d8477d23c1f0eeb0f94f5464a3a878933876
Shirsendu Das <37840366+DarkenLight@users.noreply.github.com>
2022-02-27 13:05:49 +0530

    Update setURLBase.sh

--- setURLBase.sh
+++ setURLBase.sh
@@ -1,19 +1,41 @@
-# this command will change the URL Base from "/" to /lidarr
-sed -i'.original' -e 's/<UrlBase></<UrlBase>lidarr</g' /usr/local/lidarr/config.xml
-# for setting the correct file user and group
-chown lidarr:lidarr /usr/local/lidarr/config.xml
+#!/usr/bin/env bash
 
-# this command will change the URL Base from "/" to /sonarr
-sed -i'.original' -e 's/<UrlBase></<UrlBase>sonarr</g' /usr/local/sonarr/config.xml
-# for setting the correct file user and group
-chown sonarr:sonarr /usr/local/sonarr/config.xml
+. /etc/rc.subr && load_rc_config
 
-# this command will change the URL Base from "/" to /radarr
-sed -i'.original' -e 's/<UrlBase></<UrlBase>radarr</g' /usr/local/radarr/config.xml
+function set_UrlBase()
+{
+service_name=$1
+service_home="/usr/local"
+echo "${service_name} is installed and enabled"
+# this command will change the URL Base from "/" to $service_name
+echo "Changing the URL Base of $service_name , from / to /$service_name"
+sed -i'.original' -e "s/<UrlBase></<UrlBase>${service_name}</g" ${service_home}/${service_name}/config.xml
 # for setting the correct file user and group
-chown radarr:radarr /usr/local/radarr/config.xml
+echo "setting permission to $service_home/$service_name/config.xml"
+echo
+chown -R ${service_name}:${service_name} ${service_home}/${service_name}/config.xml
+}
 
-# this command will change the URL Base from "/" to /prowlarr
-sed -i'.original' -e 's/<UrlBase></<UrlBase>prowlarr</g' /usr/local/prowlarr/config.xml
-# for setting the correct file user and group
-chown prowlarr:prowlarr /usr/local/prowlarr/config.xml
+if [ "${lidarr_enable}" == "YES" ]; then
+	set_UrlBase lidarr
+else
+	echo "Lidarr Not Enabled"
+fi
+
+if [ "${sonarr_enable}" == "YES" ]; then
+	set_UrlBase sonarr
+else
+	echo "sonarr Not Enabled"
+fi
+
+if [ "${radarr_enable}" == "YES" ]; then
+	set_UrlBase radarr
+else
+	echo "radarr Not Enabled"
+fi
+
+if [ "${prowlarr_enable}" == "YES" ]; then
+	set_UrlBase prowlarr
+else
+	echo "prowlarr Not Enabled"
+fi
